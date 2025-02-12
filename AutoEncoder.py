c3006c0d5b06f70192645523a3bff908720450c4
Ronen Somogyi-Csizmazia <somogr@uvic.ca>
2025-02-11 20:23:33 -0800

    Implemented model component and created training script

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -1,8 +1,10 @@
 
 from keras import Model
-from keras.layers import Input, Conv2D, ReLU, BatchNormalization, Flatten, Dense, Reshape, Conv2DTranspose, Activation
+from keras.layers import Input, Conv2D, ReLU, BatchNormalization, \
+    Flatten, Dense, Reshape, Conv2DTranspose, Activation
 from keras import backend as K
-
+from keras.optimizers import Adam
+from keras.losses import MeanSquaredError
 import numpy as np
 
 class AutoEncoder:
@@ -30,6 +32,7 @@ class AutoEncoder:
 
         self._num_conv_layers = len(conv_filters)
         self._shape_before_bottleneck = None
+        self._model_input = None
 
         self._build()
 
@@ -37,11 +40,41 @@ class AutoEncoder:
     def summary(self):
         self.encoder.summary()
         self.decoder.summary()
+        self.model.summary()
+
+    def compile(self, learning_rate=0.0001):
+        optimizer = Adam(learning_rate=learning_rate)
+        mse_loss =  MeanSquaredError() # Mean Squared Error
+        self.model.compile(optimizer=optimizer, loss=mse_loss)
+
+
+    def train(self, x_train, batch_size, num_epochs):
+        self.model.fit(x_train,
+                       x_train,
+                       batch_size=batch_size,
+                       epochs=num_epochs,
+                       shuffle=True)
+
+
+
+
+
 
     def _build(self):
         self._build_encoder()
         self._build_decoder()
-        #self._build_autoencoder()
+        self._build_autoencoder()
+
+
+
+    def _build_autoencoder(self):
+        model_input = self._model_input
+        model_output = self.decoder(self.encoder(model_input))
+        self.model = Model(model_input, model_output, name="autoencoder")
+
+
+
+
 
     def _build_decoder(self):
         decoder_input = self._add_decoder_input()
@@ -114,6 +147,7 @@ class AutoEncoder:
         encoder_input = self._add_encoder_input()
         conv_layers = self._add_conv_layers(encoder_input)
         bottleneck = self._add_bottleneck(conv_layers)
+        self._model_input = encoder_input # for _build_autoencoder()
         self.encoder = Model(encoder_input, bottleneck, name="encoder")
 
 
@@ -159,6 +193,8 @@ class AutoEncoder:
         x = Dense(self.latent_space_dim, name="encoder_output")(x)
         return x
     
+
+
 
 
 
